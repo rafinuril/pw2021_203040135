224115ab4ce8d81f84abe1ceeccdabcfb389c166
rafinuril <rafinafirmansyah29@gmail.com>
2021-02-16 09:13:23 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,62 @@
+<?php  
+/*
+Rafi Nuril Akbar Firmansyah
+203040135
+https://github.com/rafinuril/pw2021_203040135.git
+Pertemuan 2 - 12 Februari 2021
+Mempelajari Mengenai Sintaks PHP
+*/
+?>
+
+<?php  
+// Standard Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan Sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Rafi Nuril Akbar F";
+// echo "Halo, nama saya $nama";
+
+// Operator
+// Aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// Penggabung String / Concatenation / Concat
+// .
+// $nama_depan = "Rafi";
+// $nama_belakang = "Nuril";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assigment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Rafi";
+// $nama .= " ";
+// $nama .= "Nuril";
+// echo $nama;
+
+// Perbanding
+// <, >, <=, >=, ==
+// var_dump(1 == "1");
+
+// Identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+?>
