f4968abecdb78336d2686fe14600be34acfc933b
rafinuril <rafinafirmansyah29@gmail.com>
2021-02-23 20:55:00 +0700

    menyelesaikan video pertemuan 3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,21 @@
+<?php 
+// pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusus array
+
+// for( $i = 0; $i < 5; $i++ ) {
+// 	echo "Hello Word! <br>";
+// }
+// $i = 10;
+// while ( $i < 5 ) {
+// 	echo "Hello Word! <br>";
+// $i++;
+// }
+// $i = 10;
+// do {
+// 	echo "Hello Word! <br>";
+// $i++;
+// } while( $i < 5 );
+?>
