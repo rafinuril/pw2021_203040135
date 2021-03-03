c0d9c208ab7ecba2ffcc19a5d14d2e25799a2010
rafinuril <rafinafirmansyah29@gmail.com>
2021-03-03 17:39:43 +0700

    Menyelesaikan Modul Tugas 1

---
+++ praktikum/tugas1/latihan1a.php
@@ -0,0 +1,13 @@
+<?php
+/*
+    Rafi Nuril Akbar Firmansyah
+    203040135
+    Jumat 13.00, 26 Februari 2021
+    latihan 1A Praktikum PW
+    https://github.com/rafinuril/pw2021_203040135
+*/
+   for( $i = 1; $i <= 3; $i++ )
+        for ($j = 1; $j <= 3; $j++) {
+        echo "Ini perulangan ke ($i, $j) <br>";
+    }
+?>
