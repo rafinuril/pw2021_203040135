57e375249e9c65eeee3c0a24a7f594e831a1631d
rafinuril <rafinafirmansyah29@gmail.com>
2021-03-03 18:20:51 +0700

    Menyelesaikan Video Pertemuan 4

---
+++ pertemuan4/function.php
@@ -0,0 +1,14 @@
+<?php  
+function salam($waktu = "Datang", $nama = "Admin") {
+	return "Selamat $waktu, $nama!";
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan Function</title>
+</head>
+<body>
+	<h1><?= salam("Pagi", "Rafi Nuril"); ?></h1>
+</body>
+</html>
