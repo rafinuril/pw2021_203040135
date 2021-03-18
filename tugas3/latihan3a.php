fa5b117f4fb6de958d53e8389442b107b1c5fc0c
rafinuril <rafinafirmansyah29@gmail.com>
2021-03-18 10:38:23 +0700

    menyelesaikan tugas 3 praktikum

---
+++ tugas3/latihan3a.php
@@ -0,0 +1,12 @@
+<!-- 
+Rafi Nuril Akbar Firmansyah
+203040135
+Jam Pratikum 13:00
+github.com/rafinuril 
+-->
+
+<?php
+
+$kata = ["ada", "abel", "men", "pung", "nilai"];
+
+echo "Array $kata[0]lah suatu vari$kata[1] yang dapat $kata[2]am$kata[3] banyak $kata[4]";
